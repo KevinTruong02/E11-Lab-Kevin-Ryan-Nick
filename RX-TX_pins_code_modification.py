fd3a66039f5c0db4a57afcef7b7c1a196c9df61b
KevinTruong02 <158625214+KevinTruong02@users.noreply.github.com>
2024-02-09 16:35:21 -0800

    Update RX-TX_pins_code_modification.py

--- RX-TX_pins_code_modification.py
+++ RX-TX_pins_code_modification.py
@@ -1,58 +1,27 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
 """
-Example sketch to connect to PM2.5 sensor with either I2C or UART.
+Example sketch to connect to PM2.5 sensor with UART.
 """
 
-# pylint: disable=unused-import
 import time
 import board
 import busio
-from digitalio import DigitalInOut, Direction, Pull
 import serial
 from adafruit_pm25.uart import PM25_UART
 
-
 reset_pin = None
-# If you have a GPIO, its not a bad idea to connect it to the RESET pin
-# reset_pin = DigitalInOut(board.G0)
-# reset_pin.direction = Direction.OUTPUT
-# reset_pin.value = False
 
-
-# For use with a computer running Windows:
-# import serial
-# uart = serial.Serial("COM30", baudrate=9600, timeout=1)
-
-# For use with microcontroller board:
-# (Connect the sensor TX pin to the board/computer RX pin)
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
-# For use with Raspberry Pi/Linux:
-# import serial
-# uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
-
-# For use with USB-to-serial cable:
-# import serial
-# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
-
-# Connect to a PM2.5 sensor over UART
 pm25 = PM25_UART(uart, reset_pin)
-
-# Create library object, use 'slow' 100KHz frequency!
-#i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-# Connect to a PM2.5 sensor over I2C
-#pm25 = PM25_I2C(i2c, reset_pin)
 
 print("Found PM2.5 sensor, reading data...")
 
+start_time = time.time() 
 while True:
-    time.sleep(1)
-
     try:
         aqdata = pm25.read()
-        # print(aqdata)
     except RuntimeError:
         print("Unable to read from sensor, retrying...")
         continue
@@ -70,11 +39,20 @@ while True:
         "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
         % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
     )
-    print("---------------------------------------")
-    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
-    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
-    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
-    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
-    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
-    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
-    print("---------------------------------------")
+    #print("---------------------------------------")
+    #print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+    #print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+    #print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+    #print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+    #print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+    #print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+    #print("---------------------------------------")
+
+    elapsed_time = time.time() - start_time
+    print(elapsed_time)
+    if elapsed_time >= 5:
+        print("5 seconds elapsed. Exiting loop.")
+        break
+
+    time.sleep(1)  
+
